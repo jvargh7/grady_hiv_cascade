1c7837b4222e25a1e25411a54dc7fc6c04ce4c32
Guo <JGUO258@emory.edu>
2024-07-17 15:20:46 -0400

    Created Figure 1 with age-standardized rates.

---
+++ paper/figure_diabetes age-standardised care continuum for race and sexual orientation.R
@@ -0,0 +1,42 @@
+
+rm(list=ls());gc();source(".Rprofile")
+
+fig_df <- read_csv("diabetes/ghcd09_age standardized rates.csv") %>% 
+  mutate(prop=standardized_rate*100,lci=lower_95CI*100,uci=upper_95CI*100) %>% 
+  rename(variable=outcome,stratification=rasegrp) %>% 
+  dplyr::select(stratification,variable,prop,lci,uci) %>% 
+  bind_rows(read_csv("diabetes/ghcd05_figures care cascade.csv") %>% 
+  dplyr::filter(stratification %in% c("Total"),
+                variable %in% c("a1","a2","a3","a4")) %>% 
+  dplyr::select(-c("se", "n")) %>% 
+  mutate(variable = case_when(
+    variable == "a1" ~ "Diabetes",
+    variable == "a2" ~ "Monitoring",
+    variable == "a3" ~ "Treatment",
+    variable == "a4" ~ "Control"
+  ))) %>% 
+  mutate(variable = factor(variable,levels=c("Diabetes", "Monitoring", "Treatment", "Control"),
+                           labels=c("Detection","HbA1c \nMonitored",
+                                    "Treated","Controlled"))) %>% 
+  mutate(stratification = factor(stratification,levels=c("Total","Black Heterosexual Men","Black Sexual Minority Men",
+                                                         "Non-Black Heterosexual Men","Non-Black Sexual Minority Men"),
+                                 labels=c("Total","Black \nHeterosexual Men","Black \nSexual Minority Men",
+                                          "Non-Black \nHeterosexual Men","Non-Black \nSexual Minority Men")))
+
+
+fig_A = fig_df %>% 
+  ggplot(data=.,aes(x=stratification,y=prop,ymin=lci,ymax=uci,fill=variable)) +
+  geom_col(position = position_dodge(width = 0.9)) +
+  geom_errorbar(position = position_dodge(width = 0.9),width = 0.1) +
+  geom_text(aes(y = prop/2,label=round(prop,1)),position = position_dodge(width = 0.9)) +
+  theme_bw() +
+  scale_fill_manual(name="",values=c("red","#56B4E9","#E69F00","#009E73")) +
+  scale_y_continuous(limits=c(0,20),breaks=seq(0,20,5)) +
+  xlab("") +ylab("Proportion (%)") +
+  theme(legend.position = "bottom",
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14))
+
+ggsave(fig_A,filename=paste0(path_grady_hiv_cascade_folder,"/figures/age-standardised diabetes care continuum by race and sexual orientation.png"),width=10, height = 5)
+
